44d7c8751ab4cdd3e4febf11a5b849e46f52858c
DiegoEGarciaG <89925554+DiegoEGarciaG@users.noreply.github.com>
2021-11-22 12:18:13 -0300

    Update augmentations.py
    Se optimiza la función

--- Train_picsmodifiers/augmentations.py
+++ Train_picsmodifiers/augmentations.py
@@ -11,19 +11,11 @@ obj_images = os.listdir(obj_dir)
 obj_dir2 = '/content/Most-Wanted-/Duckies/labels_sin_aumentacion/'
 obj_labels = os.listdir(obj_dir)
 
-funciones = [A.RandomSnow(snow_point_lower=0.3, snow_point_upper=0.7, brightness_coeff=1, always_apply=False, p=1), # Agrega nieve
-    A.Blur(blur_limit=7, always_apply=False, p=1), # Difuminacion
-    A.Rotate(limit=180, interpolation=1, border_mode=4, value=None, mask_value=None, always_apply=False, p=1), # Rota la imagen en un angulo aleatorio 
-    A.Flip(p=1), # Gira la imagen horizontalmente o verticalmente o ambas
-    A.Downscale(scale_min=0.7, scale_max=0.7, interpolation=0, always_apply=False, p=1), # Le baja la calidad a la imagen
-    A.GaussNoise(var_limit=(25.0, 55.0), mean=0, per_channel=True, always_apply=False, p=1), # Aplica ruido gaussiano, que es como para hacer unos puntitos que dajan mas borrosa la imagen
-    A.ColorJitter(brightness=[0.2,1.8], contrast=[0.2,1.8], saturation=[0.2,1.8], hue=0.35, always_apply=False, p=1), # Cambia brillo, contraste, saturacion y el tono
-    A.RandomFog(fog_coef_lower=0.3, fog_coef_upper=0.3, alpha_coef=0.08, always_apply=False, p=1), # Agrega neblina
-    A.RandomShadow(shadow_roi=(0, 0, 1, 1), num_shadows_lower=1, num_shadows_upper=2, shadow_dimension=5, always_apply=False, p=1), # Agrega sombra
-    A.RandomToneCurve(scale=0.5, always_apply=False, p=1), # Cambia la razon entre las partes mas clara y mas oscuras de la imagen
-    A.ToGray(p=1), # Cambia la imagen a escala de grises bajo ciertas condiciones
-    A.ToSepia(always_apply=False, p=1)]
 # Aqui le aplicamos la transformacion
+giros = [A.RandomRotate90(p=0.5), A.Flip(p=0.5)]
+tonos = [A.Blur(blur_limit=7, always_apply=False, p=0.5),A.ColorJitter(brightness=[0.2,1.8], contrast=[0.2,1.8], saturation=[0.2,1.8], hue=0.35, always_apply=False, p=0.5),A.RandomToneCurve(scale=0.5, always_apply=False, p=0.5),A.ToGray(p=1),A.ToSepia(always_apply=False, p=0.5)]
+extras = [A.RandomSnow(snow_point_lower=0.3, snow_point_upper=0.7, brightness_coeff=1, always_apply=False),A.GaussNoise(var_limit=(25.0, 55.0), mean=0, per_channel=True, always_apply=False),A.RandomFog(fog_coef_lower=0.3, fog_coef_upper=0.3, alpha_coef=0.08, always_apply=False, p=0.5),A.RandomShadow(shadow_roi=(0, 0, 1, 1), num_shadows_lower=1, num_shadows_upper=2, shadow_dimension=5, always_apply=False)]
+blurry = A.Blur(blur_limit=7, always_apply=False, p=0.5)
 k = 0
 for obj_path in obj_images:
     img = Image.open(os.path.join(obj_dir, obj_path)) # Cargamos la imagen y la guardamos en una variable
@@ -45,15 +37,7 @@ for obj_path in obj_images:
             category_ids = category_ids + [0]
     bboxes = np.asarray(l)
     category_ids = np.array(category_ids)
-    aleatorio = random.randint(1,3)
-    anterior = []
-    composición = []
-    for i in range(aleatorio):
-        rndm = random.randint(0,11)
-        while rndm in anterior:
-            rndm = random.randint(0,11)
-        anterior = anterior + [rndm]
-        composición = composición + [funciones[rndm]]
+    composición = [A.OneOf([A.Sequential([A.OneOf(tonos),A.OneOf(extras)]),A.Sequential([A.OneOf(giros),A.OneOf(tonos),A.OneOf(extras),blurry]), A.Sequential([A.OneOf(giros),A.OneOf(tonos),blurry]), A.Sequential([A.OneOf(tonos),blurry])])]
     transformacion = A.Compose(composición,bbox_params=A.BboxParams(format='yolo', label_fields=['category_ids']))
     transformada = transformacion(image=img, bboxes=bboxes, category_ids=category_ids)# Le aplicamos la transformacion a la imagen anterior y a sus bboxes, entrega un diccionario con 2 entradas, que son la imagen y sus bboxes
     img_final = transformada["image"] # Extraemos dicha imagen del diccionario
@@ -61,17 +45,32 @@ for obj_path in obj_images:
     label = transformada["bboxes"]
     img_final.save('/content/Most-Wanted-/Duckies/i_patitos_aumentados/' + 'aug' + str(k).zfill(4) + '.jpg') # Guardamos la imagen_final en una carpeta
     contador = 0
+    width, height = Image.open(os.path.join('/content/Most-Wanted-/Duckies/i_patitos_aumentados/', 'aug' + str(k).zfill(4) + '.jpg')).size
     for i in label:
         c_x = i[0]
         c_y = i[1]
         w_obj = i[2]
         h_obj = i[3]
+        rand_x = c_x - w_obj//2
+        rand_y = c_y - h_obj//2
+        if rand_x + w_obj > width:
+            w_obj = width - rand_x
+        elif rand_x < 0:
+            w_obj = rand_x + w_obj
+            rand_x = 0
+        if rand_y + h_obj > height:
+            h_obj = height - rand_y
+        elif rand_y < 0:
+            h_obj = rand_y + h_obj
+            rand_y = 0
+        c_x = rand_x + w_obj // 2
+        c_y = rand_y + h_obj // 2
         if contador == 0:
             with open('/content/Most-Wanted-/Duckies/labels_sin_aumentacion/' + 'aug' + str(k).zfill(4) + '.txt', 'w') as f:
                     f.write('0 ' + str(c_x) + ' ' + str(c_y) + ' ' + str(w_obj) + ' ' + str(h_obj) + '\n')
         else:
             with open('/content/Most-Wanted-/Duckies/labels_sin_aumentacion/' + 'aug' + str(k).zfill(4) + '.txt', 'a') as f:
-                    f.write( '0 ' + str(c_x) + ' ' + str(c_y) + ' ' + str(w_obj) + ' ' + str(h_obj))
+                    f.write( '0 ' + str(c_x) + ' ' + str(c_y) + ' ' + str(w_obj) + ' ' + str(h_obj) + '\n')
         contador += 1
     
     k+=1
